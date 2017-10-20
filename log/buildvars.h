211515e79a703a3aa75c4ff37a8ea2d67c99247c
Boy van Amstel <boy@boyvanamstel.nl>
2017-10-20 17:48:55 +0200

    Add print screen button

--- log/buildvars.h
+++ log/buildvars.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2013-2015 by Jacob Alexander
+/* Copyright (C) 2013-2016 by Jacob Alexander
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -28,22 +28,22 @@
 // ----- Defines -----
 
 // You can change these to give your code its own name.
-#define STR_MANUFACTURER        L"Kiibohd"
+#define STR_MANUFACTURER        L"Input Club"
 #define STR_PRODUCT             L"Keyboard - WhiteFox PartialMap pjrcUSB full"
-#define STR_SERIAL              L"Clean master - 2016-05-31 00:40:10 -0700"
+#define STR_SERIAL              L"XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX - mk20dx256vlh7"
 
 
 // Strings used in the CLI module
-#define CLI_Revision            "358d480ea9862106f851cbb68c1d68da7369ddd5"
+#define CLI_Revision            "220632184329f06a4f9f8e6a5317d23e7305bd7e"
 #define CLI_Branch              "master"
 #define CLI_ModifiedStatus      "Clean"
 #define CLI_ModifiedFiles       "\r\n\t"
 #define CLI_RepoOrigin          "https://github.com/kiibohd/controller.git"
-#define CLI_CommitDate          "2016-05-31 00:40:10 -0700"
+#define CLI_CommitDate          "2017-02-20 16:38:15 -0800"
 #define CLI_CommitAuthor        "Jacob Alexander <haata@kiibohd.com>"
 #define CLI_Modules             "Scan(WhiteFox) Macro(PartialMap) Output(pjrcUSB) Debug(full)"
-#define CLI_BuildDate           "2016-07-04 20:01:49 +0000"
-#define CLI_BuildOS             "Linux-4.1.5-x86_64-linode61"
+#define CLI_BuildDate           "2017-10-20 15:47:18 +0000"
+#define CLI_BuildOS             "Linux-4.9.7-x86_64-linode80"
 #define CLI_Arch                "arm"
 #define CLI_Chip                "mk20dx256vlh7"
 #define CLI_CPU                 "cortex-m4"
@@ -56,4 +56,5 @@
 // match the INF file.
 #define VENDOR_ID               0x1C11
 #define PRODUCT_ID              0xB04D
+#define BCD_VERSION             509
 
